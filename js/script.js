b76be526f372a4e23f3f02f9d72db974d02012cd
Mykola Rudenko <m.rudenko.rb@gmail.com>
2022-09-13 16:29:50 +0300

    fix calc + localStorage

--- js/script.js
+++ js/script.js
@@ -603,12 +603,56 @@ window.addEventListener("DOMContentLoaded", () => {
   //
   //
   const result = document.querySelector(".calculating__result span");
-  let gender = "female",
-    ratio = "1.75",
-    height,
-    weight,
-    age;
 
+  let gender, ratio, height, weight, age;
+
+  // localStorage
+  //   якщо в пам'яті уже є вибраний гендер, то прописую його в перемінну
+  if (localStorage.getItem("gender")) {
+    gender = localStorage.getItem("gender");
+  } else {
+    // якщо немає, то задаю стандартний і прописую його в пам'ять
+    gender = "female";
+    localStorage.setItem("gender", "female");
+  }
+
+  //   так само з активністю
+  if (localStorage.getItem("ratio")) {
+    gender = localStorage.getItem("ratio");
+  } else {
+    gender = 1.375;
+    localStorage.setItem("ratio", 1.375);
+  }
+
+  //
+  function initLocalSettings(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach((element) => {
+      // delete active class
+      element.classList.remove(activeClass);
+      // якщо в едемента атрибут такий як у лС (тобто в лС уже був записаний) то:
+      if (element.getAttribute("id") === localStorage.getItem("gender")) {
+        // даю активний клас цьому елементу
+        element.classList.add(activeClass);
+      }
+      // для активності
+      if (
+        element.getAttribute("data-ratio") === localStorage.getItem("ratio")
+      ) {
+        // даю активний клас цьому елементу
+        element.classList.add(activeClass);
+      }
+    });
+  }
+
+  initLocalSettings("#gender div", "calculating__choose-item_active");
+  initLocalSettings(
+    ".calculating__choose_big div",
+    "calculating__choose-item_active"
+  );
+
+  // рахує
   function calcTotal() {
     // якщо чогось немає, то виводим повідомлення і перериваєм
     if (!gender || !height || !weight || !age || !ratio) {
@@ -633,9 +677,9 @@ window.addEventListener("DOMContentLoaded", () => {
   calcTotal();
 
   // для класів і атрибутів
-  function getStaticInf(parentSel, activeClass) {
+  function getStaticInf(selector, activeClass) {
     // отримую діви з цього елементу
-    const elements = document.querySelectorAll(`${parentSel} div`);
+    const elements = document.querySelectorAll(selector);
 
     // на кожен елемент навішую подію
     elements.forEach((element) => {
@@ -644,8 +688,12 @@ window.addEventListener("DOMContentLoaded", () => {
         if (e.target.getAttribute("data-ratio")) {
           // присвоюю значення цій перемінній як у атрибута
           ratio = +e.target.getAttribute("data-ratio");
+          // записую в локал сторедж ключ і значення (те що нажав користувач в калькуляторі (від 1.2 до 1.75))
+          localStorage.setItem("ratio", +e.target.getAttribute("data-ratio"));
         } else {
           gender = e.target.getAttribute("id");
+          //  зберігаю гердер який вибрали в калькуляторі
+          localStorage.setItem("gender", e.target.getAttribute("id"));
         }
         // видаляю активній клас у всіх
         elements.forEach((el) => {
@@ -661,14 +709,22 @@ window.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  getStaticInf("#gender", "calculating__choose-item_active");
-  getStaticInf(".calculating__choose_big", "calculating__choose-item_active");
+  getStaticInf("#gender div", "calculating__choose-item_active");
+  getStaticInf(".calculating__choose_big div", "calculating__choose-item_active");
 
   // ф-ція для записів значень інпуту в перемінні
   function getDynamicInf(selector) {
     const input = document.querySelector(selector);
 
     input.addEventListener("input", function (e) {
+      // якщо починаю вводити букву, інпут світитьмя червоним
+      if (input.value.match(/\D/g)) {
+        input.style.border = "2px solid red";
+        // коли цифри все норм
+      } else {
+        input.style.border = "";
+      }
+
       // чи є атрибут id => якщо і його значеення
       switch (input.getAttribute("id")) {
         // якщо рост
@@ -692,10 +748,6 @@ window.addEventListener("DOMContentLoaded", () => {
   getDynamicInf("#height");
   getDynamicInf("#weight");
   getDynamicInf("#age");
-
-
-
-
 
   //
   //
